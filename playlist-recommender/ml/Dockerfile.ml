d6caf29bff58056151eb480d889e79aa5316174e
Boyan Zhang <42496003+Johnnybyzhang@users.noreply.github.com>
2025-02-28 21:47:13 +0000

    Added dataset to ml

--- playlist-recommender/ml/Dockerfile.ml
+++ playlist-recommender/ml/Dockerfile.ml
@@ -1,5 +1,6 @@
 FROM quay.io/johnnybyzhang/uv-ml-base:latest
 WORKDIR /app
+COPY 2023_spotify_ds1.csv /app/data/dataset.csv
 COPY train_model.py .
 VOLUME /app/data
 CMD ["uv", "run", "python", "train_model.py"]
